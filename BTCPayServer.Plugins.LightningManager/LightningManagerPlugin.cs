fbc9f014998a31273c912ed2a9f35361aa7792f5
thgO.O <thgo.o.btc@proton.me>
2026-07-23 11:51:29 -0300

    chore(deps): target BTCPay Server 2.4.1

--- BTCPayServer.Plugins.LightningManager/LightningManagerPlugin.cs
+++ BTCPayServer.Plugins.LightningManager/LightningManagerPlugin.cs
@@ -11,7 +11,7 @@ public class LightningManagerPlugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     [
-        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.4.0" }
+        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.4.1" }
     ];
 
     public override void Execute(IServiceCollection services)
